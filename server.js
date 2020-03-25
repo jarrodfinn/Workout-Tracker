fe49244d15affd45e998fb38c04395a4fa78593f
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-03-25 15:26:27 -0400

    added server.js to require files

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+// routes
+app.use(require("./routes/api.js"));
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
