e6688899d61d225282f9068cefa83ae731f99e54
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-03-26 15:20:56 -0400

    added routes files

---
+++ routes/api.js
@@ -0,0 +1,44 @@
+const Workout = require("../models/workout.js");
+
+module.exports = function(app) {
+    
+  app.get("/api/workouts", function(req, res) {
+    Workout.find()
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts", function(req, res) {
+    Workout.create({})
+      .then(data => res.json(data))
+      .catch(err => {
+        console.log("err", err);
+        res.json(err);
+      });
+  });
+  app.put("/api/workouts/:id", ({ params, body }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+    )
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log("err", err)
+            res.json(err)
+        })
+});
+// app.get("/api/workouts/range", function (req, res) {
+//     Workout.find({})
+//         .then(data => {
+//             res.json(data)
+//         })
+//         .catch(err => {
+//             res.json(err)
+//         })
+// });
+};
