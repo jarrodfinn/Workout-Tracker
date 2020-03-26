e0d991207fa3536f8c68e0cd3e74c2ec532e530a
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-03-26 15:22:01 -0400

    created routes files

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(morgan("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
 // routes
 app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
